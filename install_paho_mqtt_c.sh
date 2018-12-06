df6c61657cd475fc719fe85076ac2061146a1baf
fmp <fpagliughi@mindspring.com>
2018-12-06 15:14:04 -0500

    Updated Paho C build script

--- install_paho_mqtt_c.sh
+++ install_paho_mqtt_c.sh
@@ -3,17 +3,14 @@
 # Installs the matching version of Paho MQTT C library required by the C++ lib.
 #
 
+set -e
+
 git clone https://github.com/eclipse/paho.mqtt.c.git
-
-pushd paho.mqtt.c
+cd paho.mqtt.c
 git checkout v1.2.1
-mkdir build_cmake && cd build_cmake
-cmake -DPAHO_WITH_SSL=ON ..
-make
-sudo make install
+cmake -Bbuild -H. -DPAHO_WITH_SSL=ON
+sudo cmake --build build/ --target install
 sudo ldconfig
-popd
-
 exit 0
 
 
