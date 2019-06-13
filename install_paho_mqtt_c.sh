99e4be45b71e8f31c44166a0aa8801a350d7f9eb
Vitaly Ogoltsov <vitaly.ogoltsov@me.com>
2019-06-13 13:56:17 +0300

    Fixed issue when building static paho.mqtt.cpp was using incorrect library name for paho.mqtt.c.
    PahoMqttC uses '-static' suffix when build as a static library.
    
    Signed-off-by: Vitaly Ogoltsov <vitaly.ogoltsov@me.com>

--- install_paho_mqtt_c.sh
+++ install_paho_mqtt_c.sh
@@ -8,7 +8,7 @@ set -e
 git clone https://github.com/eclipse/paho.mqtt.c.git
 cd paho.mqtt.c
 git checkout v1.2.1
-cmake -Bbuild -H. -DPAHO_WITH_SSL=ON
+cmake -Bbuild -H. -DPAHO_WITH_SSL=ON -DPAHO_BUILD_STATIC=ON -DCMAKE_POSITION_INDEPENDENT_CODE=ON
 sudo cmake --build build/ --target install
 sudo ldconfig
 exit 0
