71e218397398bbeb141bef21deed0eae6741c314
fpagliughi <fpagliughi@mindspring.com>
2019-07-28 16:10:52 -0400

    Turned on unit tests in travis build

--- travis_build.sh
+++ travis_build.sh
@@ -11,20 +11,20 @@ if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   echo "Travis build dir: $TRAVIS_BUILD_DIR,  pwd: $PWD"
 
   rm -rf build/
-  cmake -Bbuild -H. -DCMAKE_CXX_COMPILER=$COMPILER -DPAHO_BUILD_SAMPLES=ON -DPAHO_BUILD_STATIC=ON -DPAHO_BUILD_DOCUMENTATION=OFF -DPAHO_WITH_SSL=OFF
+  cmake -Bbuild -H. -DCMAKE_CXX_COMPILER=$COMPILER -DPAHO_BUILD_SAMPLES=ON -DPAHO_BUILD_STATIC=ON -DPAHO_BUILD_DOCUMENTATION=OFF -DPAHO_BUILD_TESTS=ON -DPAHO_WITH_SSL=OFF
   sudo env "PATH=$PATH" cmake --build build/ --target install
 
   # Run the unit tests
-  ./build/tests/unit/unit_tests    #--success
+  ./build/tests/unit/unit_tests --success
 
   #ctest -VV --timeout 600
   #cpack --verbose
 
   rm -rf build/
-  cmake -Bbuild -H. -DCMAKE_CXX_COMPILER=$COMPILER -DPAHO_BUILD_SAMPLES=ON -DPAHO_BUILD_STATIC=ON -DPAHO_BUILD_DOCUMENTATION=OFF -DPAHO_WITH_SSL=ON
+  cmake -Bbuild -H. -DCMAKE_CXX_COMPILER=$COMPILER -DPAHO_BUILD_SAMPLES=ON -DPAHO_BUILD_STATIC=ON -DPAHO_BUILD_DOCUMENTATION=OFF -DPAHO_BUILD_TESTS=ON -DPAHO_WITH_SSL=ON
   sudo env "PATH=$PATH" cmake --build build/ --target install
 
   # Run the unit tests
-  ./build/tests/unit/unit_tests    #--success
+  ./build/tests/unit/unit_tests --success
 
 fi
