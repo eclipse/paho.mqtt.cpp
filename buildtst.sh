e6c96220f8ba239a4e47620be32d76515f6252c0
fpagliughi <fpagliughi@mindspring.com>
2019-10-10 14:04:50 -0400

    Restored CMake back to using C++11. The C++17 test was not supposed to escape into the wild.

--- buildtst.sh
+++ buildtst.sh
@@ -13,7 +13,7 @@
 # is a quick test to use locally during development.
 #
 
-COMPILERS="g++-5 g++-6 g++-7 g++-8 clang++-3.9 clang++-4.0 clang++-5.0 clang++-6.0"
+COMPILERS="g++-5 g++-6 g++-7 g++-8 clang++-3.9 clang++-4.0 clang++-5.0 clang++-6.0 clang++-7 clang++-8"
 [ "$#" -gt 0 ] && COMPILERS="$@"
 
 [ -z "${BUILD_JOBS}" ] && BUILD_JOBS=4
@@ -34,13 +34,19 @@ for COMPILER in $COMPILERS; do
 
         if ! make -j${BUILD_JOBS} ; then
             printf "\nCompilation failed for %s\n" "${COMPILER}"
-            exit 1
+            exit 2
         fi
 
-        printf "Running unit tests for %s:\n" "${COMPILER}"
-        if ! ./test/unit/paho-mqttpp-test ; then
-            printf "\nUnit test failed for %s\n" "${COMPILER}"
+        printf "Running Catch2 Unit tests for %s:\n" "${COMPILER}"
+        if ! ./test/unit/unit_tests ; then
+            printf "\nCatch2 unit test failed for %s\n" "${COMPILER}"
             exit 3
+        fi
+
+        printf "Running CppUnit tests for %s:\n" "${COMPILER}"
+        if ! ./test/cppunit/paho-mqttpp-test ; then
+            printf "\nUnit test failed for %s\n" "${COMPILER}"
+            exit 4
         fi
 
         popd &> /dev/null
@@ -49,6 +55,12 @@ for COMPILER in $COMPILERS; do
 done
 
 rm -rf buildtst-build/
-printf "\n===== All tests completed successfully =====\n\n"
+printf "\nAll builds completed successfully\n\n"
 
+if ! cppcheck --enable=all --std=c++11 --force --quiet src/*.cpp ; then
+    printf "\ncppcheck failed\n"
+    exit 5
+fi
+
+printf "\n===== All tests completed successfully =====\n\n"
 exit 0
