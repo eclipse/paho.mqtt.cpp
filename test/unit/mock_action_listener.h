035be0b867887f01e13c6165f6f52767070999d2
fpagliughi <fpagliughi@mindspring.com>
2020-10-24 15:46:10 -0400

    Added forgotten mock_action_listener

---
+++ test/unit/mock_action_listener.h
@@ -0,0 +1,53 @@
+// mock_action_listener.h
+//
+// Dummy implementation of mqtt::iaction_listener for Unit Test.
+//
+
+/*******************************************************************************
+ * Copyright (c) 2016 Guilherme M. Ferreira <guilherme.maciel.ferreira@gmail.com>
+ * Copyright (c) 2020 Frank Pagliughi <fpagliughi@mindspring.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *   http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *    Guilherme M. Ferreira - initial implementation
+ *    Frank Pagliughi - prepared for Catch2, renamed 'mock'
+ *******************************************************************************/
+
+#ifndef __mqtt_dummy_action_listener_h
+#define __mqtt_dummy_action_listener_h
+
+#include "mqtt/token.h"
+
+namespace mqtt {
+
+/////////////////////////////////////////////////////////////////////////////
+
+class mock_action_listener : public mqtt::iaction_listener
+{
+public:
+	bool on_success_called { false };
+	bool on_failure_called { false };
+
+	void on_success(const mqtt::token& token) override {
+		on_success_called = true;
+	}
+
+	void on_failure(const mqtt::token& token) override {
+		on_failure_called = true;
+	}
+
+};
+
+/////////////////////////////////////////////////////////////////////////////
+// end namespace mqtt
+}
+
+#endif //  __mqtt_dummy_action_listener_h
