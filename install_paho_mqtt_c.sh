9321cc5e6a70ac1744e7cac0fa991ebe6eb6bed4
fpagliughi <fpagliughi@mindspring.com>
2020-12-06 10:05:37 -0500

    Bumped Travis CI to use C v1.3.7

--- install_paho_mqtt_c.sh
+++ install_paho_mqtt_c.sh
@@ -7,7 +7,7 @@ set -ex
 
 git clone https://github.com/eclipse/paho.mqtt.c.git
 cd paho.mqtt.c
-git checkout v1.3.6
+git checkout v1.3.7
 
 cmake -Bbuild -H. -DPAHO_WITH_SSL=ON -DPAHO_BUILD_STATIC=ON -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DPAHO_ENABLE_TESTING=OFF
 sudo env "PATH=$PATH" cmake --build build/ --target install
