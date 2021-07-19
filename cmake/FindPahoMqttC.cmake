f2a15f4abf09004f752e9908b20c933b61d273c9
Florian Kauer <koalo@koalo.de>
2021-07-19 09:00:07 +0200

    avoid adding Paho MQTT C library twice
    Signed-off-by: Florian Kauer <koalo@koalo.de>

--- cmake/FindPahoMqttC.cmake
+++ cmake/FindPahoMqttC.cmake
@@ -17,7 +17,9 @@ find_library(PAHO_MQTT_C_LIBRARIES NAMES ${_PAHO_MQTT_C_LIB_NAME})
 unset(_PAHO_MQTT_C_LIB_NAME)
 find_path(PAHO_MQTT_C_INCLUDE_DIRS NAMES MQTTAsync.h)
 
-add_library(PahoMqttC::PahoMqttC UNKNOWN IMPORTED)
+if (NOT TARGET PahoMqttC::PahoMqttC)
+    add_library(PahoMqttC::PahoMqttC UNKNOWN IMPORTED)
+endif ()
 
 set_target_properties(PahoMqttC::PahoMqttC PROPERTIES
     IMPORTED_LOCATION "${PAHO_MQTT_C_LIBRARIES}"
