53367104cbed2dd97bedf3260b1e950f3727d6fa
Heinz-Peter Liechtenecker <heinz.liechtenecker@gmail.com>
2022-08-16 13:30:55 +0200

    Append static suffix on all platforms

--- cmake/FindPahoMqttC.cmake
+++ cmake/FindPahoMqttC.cmake
@@ -6,11 +6,9 @@ else()
     set(_PAHO_MQTT_C_LIB_NAME paho-mqtt3a)
 endif()
 
-# add suffix when using static Paho MQTT C library variant on Windows
-if(WIN32)
-    if(PAHO_BUILD_STATIC)
-        set(_PAHO_MQTT_C_LIB_NAME ${_PAHO_MQTT_C_LIB_NAME}-static)
-    endif()
+# add suffix when using static Paho MQTT C library variant 
+if(PAHO_BUILD_STATIC)
+    set(_PAHO_MQTT_C_LIB_NAME ${_PAHO_MQTT_C_LIB_NAME}-static)
 endif()
 
 if(PAHO_WITH_MQTT_C)
