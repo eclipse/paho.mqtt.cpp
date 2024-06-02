0959334148d322c8945c17011ca3f9ae2294be9b
fpagliughi <fpagliughi@mindspring.com>
2024-06-02 11:31:37 -0400

    Fixed and optimized 'topic_matcher'

--- test/unit/test_topic_matcher.cpp
+++ test/unit/test_topic_matcher.cpp
@@ -28,29 +28,29 @@ using namespace mqtt;
 
 TEST_CASE("insert/get", "[topic_matcher]")
 {
-	topic_matcher<int> matcher;
+	topic_matcher<int> tm;
 
-	matcher.insert({"some/random/topic", 42});
+	tm.insert({"some/random/topic", 42});
 
-	auto it = matcher.find("some/random/topic");
+	auto it = tm.find("some/random/topic");
 
-	REQUIRE(it != matcher.end());
+	REQUIRE(it != tm.end());
 	REQUIRE(it->first == "some/random/topic");
 	REQUIRE(it->second == 42);
 }
 
-TEST_CASE("matcher matches", "[topic_matcher]")
+TEST_CASE("matcher initialize", "[topic_matcher]")
 {
-	topic_matcher<int> matcher {
+	topic_matcher<int> tm {
 		{ "some/random/topic", 42 },
 		{ "some/#", 99 },
 		{ "some/other/topic", 55 },
 		{ "some/+/topic", 33 }
 	};
 
-	auto it = matcher.matches("some/random/topic");
+	auto it = tm.matches("some/random/topic");
 
-	for (; it != matcher.end(); ++it) {
+	for (; it != tm.matches_end(); ++it) {
 		bool ok = (
 		    (it->first == "some/random/topic" && it->second == 42) ||
 			(it->first == "some/#" &&  it->second == 99) ||
@@ -58,4 +58,35 @@ TEST_CASE("matcher matches", "[topic_matcher]")
 		);
 		REQUIRE(ok);
 	}
+}
+
+// This one is mostly borrowed from the Paho Python tests.
+// It has a number of good corner cases that shoud and should not match.
+TEST_CASE("matcher matches", "[topic_matcher]")
+{
+    // Should match
+
+    REQUIRE((topic_matcher<int>{{"foo/bar", 42}}.has_match("foo/bar")));
+    REQUIRE((topic_matcher<int>{{"foo/+", 42}}.has_match("foo/bar")));
+    REQUIRE((topic_matcher<int>{{"foo/+/baz", 42}}.has_match("foo/bar/baz")));
+    REQUIRE((topic_matcher<int>{{"foo/+/#", 42}}.has_match("foo/bar/baz")));
+    REQUIRE((topic_matcher<int>{{"A/B/+/#", 42}}.has_match("A/B/B/C")));
+    REQUIRE((topic_matcher<int>{{"#", 42}}.has_match("foo/bar/baz")));
+    REQUIRE((topic_matcher<int>{{"#", 42}}.has_match("/foo/bar")));
+    REQUIRE((topic_matcher<int>{{"/#", 42}}.has_match("/foo/bar")));
+    REQUIRE((topic_matcher<int>{{"$SYS/bar", 42}}.has_match("$SYS/bar")));
+    REQUIRE((topic_matcher<int>{{"foo/#", 42}}.has_match("foo/$bar")));
+    REQUIRE((topic_matcher<int>{{"foo/+/baz", 42}}.has_match("foo/$bar/baz")));
+
+    // Should not match
+
+    REQUIRE(!(topic_matcher<int>{{"test/6/#", 42}}.has_match("test/3")));
+    REQUIRE(!(topic_matcher<int>{{"foo/bar", 42}}.has_match("foo")));
+    REQUIRE(!(topic_matcher<int>{{"foo/+", 42}}.has_match("foo/bar/baz")));
+    REQUIRE(!(topic_matcher<int>{{"foo/+/baz", 42}}.has_match("foo/bar/bar")));
+    REQUIRE(!(topic_matcher<int>{{"foo/+/#", 42}}.has_match("fo2/bar/baz")));
+    REQUIRE(!(topic_matcher<int>{{"/#", 42}}.has_match("foo/bar")));
+    REQUIRE(!(topic_matcher<int>{{"#", 42}}.has_match("$SYS/bar")));
+    REQUIRE(!(topic_matcher<int>{{"$BOB/bar", 42}}.has_match("$SYS/bar")));
+    REQUIRE(!(topic_matcher<int>{{"+/bar", 42}}.has_match("$SYS/bar")));
 }
