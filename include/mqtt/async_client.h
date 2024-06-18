f6a3431adb809c3d2cfcddb4fc77c3f79be5e7cc
fpagliughi <fpagliughi@mindspring.com>
2024-06-18 14:14:00 -0400

    Bumped version to v1.5 prerelease

--- include/mqtt/async_client.h
+++ include/mqtt/async_client.h
@@ -54,16 +54,16 @@ namespace mqtt {
 
 #if defined(PAHO_MQTTPP_VERSIONS)
 	/** The version number for the client library. */
-	const uint32_t PAHO_MQTTPP_VERSION = 0x01040000;
+	const uint32_t PAHO_MQTTPP_VERSION = 0x01050000;
 	/** The version string for the client library  */
-	const string PAHO_MQTTPP_VERSION_STR("Paho MQTT C++ (mqttpp) v. 1.4.0");
+	const string PAHO_MQTTPP_VERSION_STR("Paho MQTT C++ (mqttpp) v. 1.5.0");
 	/** Copyright notice for the client library */
 	const string PAHO_MQTTPP_COPYRIGHT("Copyright (c) 2013-2024 Frank Pagliughi");
 #else
 	/** The version number for the client library. */
-	const uint32_t VERSION = 0x01040000;
+	const uint32_t VERSION = 0x01050000;
 	/** The version string for the client library  */
-	const string VERSION_STR("Paho MQTT C++ (mqttpp) v. 1.4.0");
+	const string VERSION_STR("Paho MQTT C++ (mqttpp) v. 1.5.0");
 	/** Copyright notice for the client library */
 	const string COPYRIGHT("Copyright (c) 2013-2024 Frank Pagliughi");
 #endif
